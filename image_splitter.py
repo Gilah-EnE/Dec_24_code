a65f41029ef610f61c974e08f979579a22cfc2db
Artem <63448341+Gilah-EnE@users.noreply.github.com>
2025-01-06 12:33:34 +0200

    Update image_splitter.py

--- image_splitter.py
+++ image_splitter.py
@@ -19,14 +19,21 @@ class GPTReader(object):
     def block_reader(self):
         return self._file
     
-def split_image(fname: str, sector_size=512):
+def split_image(fname: str, sector_size=512) -> list:
     reader = GPTReader(fname, sector_size=sector_size)
+
+    files = list()
 
     for partition in reader.partition_table.valid_entries():
         file_base_name = partition.partition_id
     
         out_file = os.path.join(os.getcwd(), f"{file_base_name}.bin")
+        files.append(out_file)
 
         with open(out_file, "wb+") as fout:
             for block in reader.block_reader.blocks_in_range(partition.first_block, partition.length):
                 fout.write(block)
+
+    return files
+
+# print(split_image('/home/gilah/Documents/test_gpt.bin'))
